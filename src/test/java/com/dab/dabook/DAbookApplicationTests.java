4da272dd08031a3a5091c874457812bb9d09665e
DABB <1433683384@qq.com>
2023-03-09 19:41:13 +0800

    原始文件

---
+++ src/test/java/com/dab/dabook/DAbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dab.dabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DAbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
