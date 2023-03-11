7dcb4020766497bb2141df64f6e6a63ca907a942
DABB <1433683384@qq.com>
2023-03-11 18:51:00 +0800

    练习文件1.2.1

--- src/main/java/com/dab/dabook/servise/impl/IBookServiseImpl.java
+++ src/main/java/com/dab/dabook/servise/impl/IBookServiseImpl.java
@@ -6,14 +6,18 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.dab.dabook.dao.BookDao;
 import com.dab.dabook.domain.Book;
 import com.dab.dabook.servise.IBookServise;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class IBookServiseImpl extends ServiceImpl<BookDao, Book> implements IBookServise {
+    //每个层级都有联系
+    @Autowired
+    private BookDao dao;
 
     @Override
     public IPage<Book> getPage(int currentPage, int pageSize) {
         IPage<Book> page = new Page<Book>(currentPage, pageSize);
-        return page;
+        return dao.selectPage(page, null);
     }
 }
